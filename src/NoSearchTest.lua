4c495ab471f5fe64aa06fc33e104618a07f395ab
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2021-04-07 21:22:34 -0400

    Create NoSearchTest.lua

---
+++ src/NoSearchTest.lua
@@ -0,0 +1,14 @@
+-- {"id":12323123,"ver":"1.0.0","libVer":"1.0.0","author":"Doomsdayrs","dep":[]}
+
+return {
+  id = 163568,
+  name = "No Search Test".
+  baseURL = "",
+  listings = {
+    Listing("Null", false, function() return {} end)
+  },
+  getPassage    = function() return ""          end,
+  parseNovel    = function() return NovelInfo() end,
+  shrinkURL     = function() return ""          end,
+  expandURL     = function() return ""          end,
+}
