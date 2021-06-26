6183b59882ef85ab586dab8ff226e79451c45e00
doomsdayrs <doomsdayrs@gmail.com>
2021-06-26 00:10:18 -0400

    Replaced spaces with tabs

--- src/en/SettingsTest.lua
+++ src/en/SettingsTest.lua
@@ -1,38 +1,36 @@
 -- {"id":191401,"ver":"1.0.0","libVer":"1.0.0","author":"Doomsdayrs","dep":[]}
 
 return {
-  id = 191401,
-  name = "Test: Extension Settings",
-  baseURL = "",
-  listings = {
-    Listing("Null", false, function() return {} end)
-  },
-  getPassage    = function() return ""          end,
-  parseNovel    = function() return NovelInfo() end,
-  shrinkURL     = function() return ""          end,
-  expandURL     = function() return ""          end,
-  hasSearch     = false,
-  settings      = {
-    TextFilter(5, "This is a text filter"),
-    SwitchFilter(6, "This is a switch filter"),
-    CheckboxFilter(7, "This is a check box filter"),
-    RadioGroupFilter(8, "This is a radio group filter", {"Choice A", "Choice B", "Choice C"}),
-    DropdownFilter(9, "This is a drop down filter", {"Choice A", "Choice B", "Choice C"}),
-    FilterGroup("This is a filter group", {
-        TextFilter(10,"TextFilter"), 
-        TextFilter(11,"TextFilter") 
-      }
-    ),
-    FilterList("This is a filter list", {
-        TextFilter(12, "TextFilter"), 
-        SwitchFilter(13, "SwitchFilter")
-      }
-    )
-  },
-  updateSetting = function(id,value) 
-    print("This is the id")
-    print(id)
-    print("This is the value")
-    print(value)
-                                                end
+	id = 191401,
+	name = "Test: Extension Settings",
+	baseURL = "",
+	listings = {
+		Listing("Null", false, function() return {} end)
+	},
+	getPassage    = function() return ""          end,
+	parseNovel    = function() return NovelInfo() end,
+	shrinkURL     = function() return ""          end,
+	expandURL     = function() return ""          end,
+	hasSearch     = false,
+	settings      = {
+		TextFilter(5, "This is a text filter"),
+		SwitchFilter(6, "This is a switch filter"),
+		CheckboxFilter(7, "This is a check box filter"),
+		RadioGroupFilter(8, "This is a radio group filter", {"Choice A", "Choice B", "Choice C"}),
+		DropdownFilter(9, "This is a drop down filter", {"Choice A", "Choice B", "Choice C"}),
+		FilterGroup("This is a filter group", {
+			TextFilter(10,"TextFilter"), 
+			TextFilter(11,"TextFilter") 
+		}),
+		FilterList("This is a filter list", {
+			TextFilter(12, "TextFilter"), 
+			SwitchFilter(13, "SwitchFilter")
+      		})
+	},
+	updateSetting = function(id,value) 
+		print("SettingsTest.lua: This is the id")
+		print(id)
+		print("SettingsTest.lua: This is the value")
+		print(value)
+	end
 }
